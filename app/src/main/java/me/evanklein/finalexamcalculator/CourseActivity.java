6091fd42308d0b5467ceaccb650ffca637b771d8
evanklein1 <evan.klein@hotmail.com>
2015-11-15 17:37:07 -0500

    removed default nav bar from activity course

--- app/src/main/java/me/evanklein/finalexamcalculator/CourseActivity.java
+++ app/src/main/java/me/evanklein/finalexamcalculator/CourseActivity.java
@@ -184,15 +184,7 @@ public class CourseActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camara) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
+        if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
 
@@ -439,7 +431,7 @@ public class CourseActivity extends AppCompatActivity
         }
     }
 
-    public void saveCourse() {
+    public void saveCourse(View view) {
         student.addCourse(course);
         if (newCourse) {
             addCourseToDB();
