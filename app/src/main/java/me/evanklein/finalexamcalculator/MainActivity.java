8f5953e69997e2faf3b61db3edb04ec77b2731f4
evanklein1 <evan.klein@hotmail.com>
2015-11-15 14:18:04 -0500

    added custom toolbar and removed default one

--- app/src/main/java/me/evanklein/finalexamcalculator/MainActivity.java
+++ app/src/main/java/me/evanklein/finalexamcalculator/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
